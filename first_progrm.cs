599473f2e188d7822cec4dd5d0f615baf2107980
Kevin Pearson <krp@cswnet.com>
2019-08-26 14:51:45 -0500

    changing to fully qualified name'

--- first_progrm.cs
+++ first_progrm.cs
@@ -1,4 +1,4 @@
-using System;
+
 // using System.Collections.Generic;
 // using System.Linq;
 // using System.Text;
@@ -11,7 +11,7 @@ namespace HelloWorld
 		static void Main(string[] args)
 		{
 
-			Console.WriteLine("Hello World!");
+			System.Console.WriteLine("Hello World!");
 
 		}
 	}
